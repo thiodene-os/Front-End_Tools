1de4af3b8bbce78d3151b5854f9f55a3e8b3c2ae
Serge Ayissi <ayissi.serge@gmail.com>
2018-01-26 15:19:26 -0500

    Create date_picker.js

---
+++ date_picker/date_picker.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+  $(function()
+  {
+    $("#item_receive_date").datepicker(
+    {
+      showOn: 'button'
+      ,buttonImage: '/~plugin_folder~/jquery_ui/images/calendar.gif'
+      ,buttonImageOnly: true
+      ,dateFormat: 'yy-mm-dd'
+      ,changeMonth: true
+      ,changeYear: true
+    }) ;         
+  }) ; // document.ready
+</script>
