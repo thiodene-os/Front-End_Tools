b8b199cad445f209f17f10f71c03965ecfcf5fba
Serge Ayissi <ayissi.serge@gmail.com>
2018-02-01 11:17:15 -0500

    Create build_medium_side_banner.php

---
+++ google_adsense/build_medium_side_banner.php
@@ -0,0 +1,44 @@
+<?php
+
+$mediumsidead_block = "<table width=\"320\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" class=\"lateent\">
+  <tr bgcolor=\"#F0F0F0\">
+    <td width=\"10\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"300\" height=\"15\" align=\"center\" valign=\"middle\"><span class=\"advert\">ADVERTISEMENT</span></td>
+    <td width=\"10\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+  </tr>
+  <tr bgcolor=\"#F0F0F0\">
+    <td width=\"10\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"300\" align=\"center\" valign=\"middle\"><script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
+<!-- sidebig_forevent -->
+<ins class=\"adsbygoogle\"
+     style=\"display:inline-block;width:300px;height:250px\"
+     data-ad-client=\"ca-pub-7983318394533936\"
+     data-ad-slot=\"3023578005\"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></td>
+    <td width=\"10\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+  </tr>
+  <tr bgcolor=\"#F0F0F0\">
+    <td width=\"10\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"300\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"10\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+  </tr>
+  <tr>
+    <td width=\"10\" height=\"25\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"300\" height=\"25\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"10\" height=\"25\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+  </tr>
+</table>";
+
+/* Example of HTML use of the Google add
+
+</table></td>
+<td width="20"><img src="/bce/spacer.gif" width="1" height="1"></td>
+  <td width="320" height="600" align="left" valign="top"><?php echo $mediumsidead_block; ?><span class="selabout"><?php echo $estatus; ?><br><?php echo $eturl_block; ?><br><?php echo $spurl_block; ?><?php echo $loc_block; ?><?php echo $date_block; ?><br><?php echo $agent_block; ?><br><?php echo $ticket_block; ?></span></td></tr>
+</table></td>
+  </tr>
+
+*/
+
+?>
