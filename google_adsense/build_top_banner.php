e406123ac993a2b16ffe1e6c99e3cf61a1fa628f
Serge Ayissi <ayissi.serge@gmail.com>
2018-02-01 11:13:06 -0500

    Create build_top_banner.php

---
+++ google_adsense/build_top_banner.php
@@ -0,0 +1,50 @@
+<?php
+
+// Top Banner Ad
+
+$topbannerad_block = "<table width=\"940\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" class=\"lateent\">
+  <tr>
+    <td width=\"106\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"728\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"106\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+  </tr>
+  <tr bgcolor=\"#F0F0F0\">
+    <td width=\"106\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"728\" height=\"15\" align=\"center\" valign=\"middle\"><span class=\"advert\">ADVERTISEMENT</span></td>
+    <td width=\"106\" height=\"15\" align=\"center\" valign=\"middle\"><img src\=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+  </tr>
+  <tr bgcolor=\"#F0F0F0\">
+    <td width=\"106\" height=\"90\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"728\" height=\"90\" align=\"center\" valign=\"middle\"><script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
+<!-- sidebig_forevent -->
+<ins class=\"adsbygoogle\"
+     style=\"display:inline-block;width:728px;height:90px\"
+     data-ad-client=\"ca-pub-7983318394533936\"
+     data-ad-slot=\"3023578005\"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></td>
+    <td width=\"106\" height=\"90\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+  </tr>
+  <tr bgcolor=\"#F0F0F0\">
+    <td width=\"106\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"728\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"106\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+  </tr>
+  <tr>
+    <td width=\"106\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"728\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+    <td width=\"106\" height=\"15\" align=\"center\" valign=\"middle\"><img src=\"/bce/spacer.gif\" width=\"1\" height=\"1\"></td>
+  </tr>
+</table>" ;
+
+/* example of HTML part for displaying of the Ad
+
+<table width="940" border="0" cellspacing="0" cellpadding="0">
+			  <tr>
+			  	<td width="100%" height="130"><?php echo $topbannerad_block; ?></td>
+			  </tr>
+*/
+
+
+?>
