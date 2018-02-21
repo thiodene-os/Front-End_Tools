f6c2f2e35ee6c1a737bb5cbf03a317b79a69a3e9
Serge Ayissi <ayissi.serge@gmail.com>
2018-02-21 09:52:24 -0500

    Create iframe.js

---
+++ google_site_location/js/iframe.js
@@ -0,0 +1,1 @@
+<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2886.7992746816744!2d-79.67020398431676!3d43.65234436050367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s3-6350+Tomken+Road%2C+Mississauga%2C+ON%2C+L5T+1Y3!5e0!3m2!1sen!2sca!4v1519222739171" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
